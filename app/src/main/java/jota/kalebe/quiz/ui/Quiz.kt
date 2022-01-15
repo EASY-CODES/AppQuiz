11a8f201b602b185ae0b65874f2516a5d88c1b13
jkalebe <jeremiaskalebe@gmail.com>
2022-01-15 16:00:10 -0300

    first commit

---
+++ app/src/main/java/jota/kalebe/quiz/ui/Quiz.kt
@@ -0,0 +1,12 @@
+package jota.kalebe.quiz.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jota.kalebe.quiz.R
+
+class Quiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
