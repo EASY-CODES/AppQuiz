7a44c59d2bee1c1b0c84211b4d4562bc1796d40d
jkalebe <jeremiaskalebe@gmail.com>
2022-01-16 23:42:07 -0300

    Telas de Quiz E Result adicionadas

--- app/src/main/java/jota/kalebe/quiz/ui/Result.kt
+++ app/src/main/java/jota/kalebe/quiz/ui/Result.kt
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import jota.kalebe.quiz.R
 
-class MainActivity : AppCompatActivity() {
+class Result : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_result)
     }
 }
