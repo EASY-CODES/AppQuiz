25a8c19018a8633a92f305aba732ed27e95e00e3
jkalebe <jeremiaskalebe@gmail.com>
2022-01-17 00:49:03 -0300

    Funções de clicks adicionados

--- app/src/main/java/jota/kalebe/quiz/ui/MainActivity.kt
+++ app/src/main/java/jota/kalebe/quiz/ui/MainActivity.kt
@@ -1,14 +1,40 @@
 package jota.kalebe.quiz.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TableLayout
+import androidx.core.view.get
 import jota.kalebe.quiz.R
 
 class MainActivity : AppCompatActivity() {
+    private val CATEGORY = "CATEGORY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tableLayout: TableLayout = findViewById(R.id.tablelayout)
 
+        openActivity(tableLayout,0, R.id.category1, "Math")
+        openActivity(tableLayout,0, R.id.category2, "Music")
+        openActivity(tableLayout,0, R.id.category3, "Movie")
+        openActivity(tableLayout,1, R.id.category4, "Science")
+        openActivity(tableLayout,1, R.id.category5, "Language")
+        openActivity(tableLayout,1, R.id.category6, "Geography")
+
+
+
+    }
+
+    fun openActivity(tableLayout: TableLayout , row: Int, position: Int, category: String){
+        val row: View = tableLayout[row].findViewById(position)
+        row.setOnClickListener {
+            intent = Intent(this, Quiz::class.java)
+            intent.putExtra(CATEGORY, category)
+            this.startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/jota/kalebe/quiz/ui/Quiz.kt
+++ app/src/main/java/jota/kalebe/quiz/ui/Quiz.kt
@@ -1,12 +1,27 @@
 package jota.kalebe.quiz.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import jota.kalebe.quiz.R
 
 class Quiz : AppCompatActivity() {
+    private val CATEGORY = "CATEGORY"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        val categoryTitle = intent.getStringExtra(CATEGORY)
+
+        val textTitle: TextView = findViewById(R.id.titleCategory)
+        textTitle.text = "$categoryTitle Quiz"
+
+        val btNext: Button = findViewById(R.id.btNext)
+        btNext.setOnClickListener {
+            intent = Intent(this, Result::class.java)
+            this.startActivity(intent)
+        }
     }
 }
